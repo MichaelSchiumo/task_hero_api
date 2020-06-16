d1ff1c451b77a2f06c6a63c82aa3cfa48395b86b
Michael Schiumo <mschiumo18@gmail.com>
2020-06-16 18:59:35 -0400

    added task model

---
+++ db/migrate/20200616225527_create_tasks.rb
@@ -0,0 +1,12 @@
+class CreateTasks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string :name
+      t.string :deadline
+      t.string :category
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
