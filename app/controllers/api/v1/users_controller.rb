b02f3d9bfa9197b8b0835c33f82d9ab878b63f0b
Michael Schiumo <mschiumo18@gmail.com>
2020-06-16 21:24:14 -0400

    added show action to users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,15 +5,19 @@ class Api::V1::UsersController < ApplicationController
     render json: { user: UserSerializer.new(current_user)}, status: accepted
   end
 
-    def create
-      @user = User.create(user_params)
-      if @user.valid?
-        @token = encode_token(user_id: @user.id)
-        render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
-      else
-        render json: { error: 'Failed to create user.' }, status: :not_acceptable
-      end
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.valid?
+       @token = encode_token(user_id: @user.id)
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+    else
+      render json: { error: 'Failed to create user.' }, status: :not_acceptable
     end
+  end
   
     private
     def user_params
