b2b6280c7581307c332d2a291bd19148ce0d4b4d
Michael Schiumo <mschiumo18@gmail.com>
2020-06-16 22:01:06 -0400

    login user request works with token

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:create, :show, :index, :update, :destroy]
+      resources :tasks, only: [:create, :show, :index, :update, :destroy]
       post '/login', to: 'auth#create'
       get '/profile', to: 'users#profile'
     end
