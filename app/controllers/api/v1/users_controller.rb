9a084928d4209aa40dfabd0344139363f4222025
Michael Schiumo <mschiumo18@gmail.com>
2020-06-19 14:40:41 -0400

    checking

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,6 +9,10 @@ class Api::V1::UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
+  def index 
+    @users = User.all
+  end
+
   def create
     @user = User.create(user_params)
     if @user.valid?
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
     has_secure_password
     has_many :tasks
     validates :username, uniqueness: true
-    validates :bio, presence: true
     validates :username, length: { minimum: 4 }
     
 end
