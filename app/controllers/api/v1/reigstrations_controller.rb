32aff9ceede12e9f6396328ec7da998611ed841f
Michael Schiumo <michael@Michaels-MacBook-Pro-3.local>
2020-06-23 00:07:40 -0400

    fixed user variables in controllers

--- app/controllers/api/v1/reigstrations_controller.rb
+++ app/controllers/api/v1/reigstrations_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::RegistrationsController < ApplicationController 
     def create 
-        user = User.create!(username: params['user']['email'], 
+        @user = User.create!(username: params['user']['email'], 
             password: params['user']['password'],
             password_confirmation: params['user']['password_confirmation']
         )
 
-        if user 
-            session[:user_id] = user.id 
+        if @user 
+            session[:user_id] = @user.id 
             render json: { status: :created, user: user }
         else 
             render json: { status: 500 }
